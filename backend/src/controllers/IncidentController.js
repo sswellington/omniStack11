e3153895a14ef8cbc5e0c5d1e4fd6dd3cbfa4deb
sswellington <wellingtons@id.uff.br>
2020-03-26 00:58:53 -0300

    feat: pages

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -4,32 +4,49 @@
 const connection = require('../database/connection');
 
 module.exports = {
-    
+
     async index(request, response) {
-        const incidents = await connection('incidents').select('*');
+
+        const { page = 1 } = request.query;
+
+        const [count] = await connection('incidents').count();
+
+        const incidents = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+            .limit(5)
+            .offset((page - 1) * 5)
+            .select([
+                'incidents.*', 
+                'ongs.name',
+                'ongs.email',
+                'ongs.whatsapp',
+                'ongs.city',
+                'ongs.uf'
+            ]);
+
+        response.header('X-Total-Count', count['count(*)']);
+
         return response.json(incidents);
     },
 
     async create(request, response) {
 
-        const { title, description, value } = request.body;        
+        const { title, description, value } = request.body;
         const ong_id = request.headers.authorization;
-
         const [id] = await connection('incidents').insert({
             title,
             description,
             value,
             ong_id,
-        }); 
+        });
 
         return response.json({ id });
     },
 
     async delete(request, response) {
 
-        const {id} = request.params;
+        const { id } = request.params;
         const ong_id = request.headers.authorization;
-
         const incidents = await connection('incidents')
             .where('id', id)
             .select('ong_id')
@@ -40,7 +57,6 @@ module.exports = {
         }
 
         await connection('incidents').where('id', id).delete();
-
         return response.status(204).send();
     },
 };
