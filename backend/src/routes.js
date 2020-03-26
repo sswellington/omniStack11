c7f679ab0f13f0eaeba566558319f774c50fc859
sswellington <wellingtons@id.uff.br>
2020-03-26 00:39:24 -0300

    feat: Profile and Session

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,14 +4,20 @@ const express = require('express');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.get('/ongs', OngController.index);   // Cadastrar
-routes.post('/ongs', OngController.create); // Listar
+routes.post('/sessions', SessionController.create);
 
-routes.get('/incidents', IncidentController.index);         // Cadastrar
-routes.post('/incidents', IncidentController.create);       // Listar
+routes.get('/ongs', OngController.index);   // Listar
+routes.post('/ongs', OngController.create); // Cadastrar
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);         // Listar
+routes.post('/incidents', IncidentController.create);       // Cadastrar
 routes.delete('/incidents/:id', IncidentController.delete); // Apagar
 
 module.exports = routes;
