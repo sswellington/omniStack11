42360863b679644ed5359172dac87bfcfdb369db
sswellington <wellingtons@id.uff.br>
2020-03-27 12:24:15 -0300

    feat: NewIncident

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 
 export default function NewIncident() {
-    return (<h1>NewIncident</h1>);
+    return (
+        <div className="new-incident-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be The Hero" />
+                    <h1> Cadastrar novo caso </h1>
+                    <p> Descreve o caso detalhadamente para encontrar um herói para revolver isso.</p>
+                    <Link className="back-link" to="/profile"> <FiArrowLeft size={16} color="#E02041" />
+                        Voltar para Home
+                    </Link>
+                </section>
+                <form>
+                    <input placeholder="Título do caso" />
+                    <textarea placeholder="Descrição" />
+                    <input placeholder="Valor em reais" />
+
+                    <button className="button" type="submit">Cadastrar</button>
+                </form>
+            </div>
+        </div>
+    );
 }
